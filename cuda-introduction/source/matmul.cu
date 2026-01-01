d8b7616245639b7c665de182ff0591ada40b1c2f
kongtiaochengtailang <1579924557@qq.com>
2026-01-01 10:49:51 +0800

    finish this homework

--- cuda-introduction/source/matmul.cu
+++ cuda-introduction/source/matmul.cu
@@ -1,5 +1,6 @@
 #include "common.h"
 
+#include <cstddef>
 #include <device_launch_parameters.h>
 
 #include <cmath>
@@ -12,17 +13,22 @@ __global__ void matrixMultiplicationNaive(float* const matrixP, const float* con
 {
     // TODO 10a: Compute the P matrix global index for each thread along x and y dimentions.
     // Remember that each thread of the kernel computes the result of 1 unique element of P
-    unsigned px;
-    unsigned py;
+    unsigned px = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned py = blockIdx.y * blockDim.y + threadIdx.y;
 
     // TODO 10b: Check if px or py are out of bounds. If they are, return.
+    if (px >= sizeMX or py >= sizeNY)
+        return;
 
     // TODO 10c: Compute the dot product for the P element in each thread
     // This loop will be the same as the host loop
     float dot = 0.0;
+    for (int k = 0; k < sizeXY; ++k) {
+        dot += matrixM[px * sizeXY + k] * matrixN[k * sizeNY + py];
+    }
 
     // TODO 10d: Copy dot to P matrix
-    // matrixP[] = dot;
+    matrixP[py * sizeMX + px] = dot;
 }
 
 int main(int argc, char *argv[])
@@ -31,19 +37,19 @@ int main(int argc, char *argv[])
     // Then try large multiple-block square matrix like 64x64 up to 2048x2048.
     // Then try square, non-power-of-two like 15x15, 33x33, 67x67, 123x123, and 771x771
     // Then try rectangles with powers of two and then non-power-of-two.
-    const unsigned sizeMX = 0;
-    const unsigned sizeXY = 0;
-    const unsigned sizeNY = 0;
+    const unsigned sizeMX = 123;
+    const unsigned sizeXY = 123;
+    const unsigned sizeNY = 123;
 
     // TODO 2: Allocate host 1D arrays for:
     // matrixM[sizeMX, sizeXY]
     // matrixN[sizeXY, sizeNY]
     // matrixP[sizeMX, sizeNY]
     // matrixPGold[sizeMX, sizeNY]
-    float* matrixM;
-    float* matrixN;
-    float* matrixP;
-    float* matrixPGold;
+    float* matrixM = new float[sizeMX * sizeXY];
+    float* matrixN = new float[sizeXY * sizeNY];
+    float* matrixP = new float[sizeMX * sizeNY];
+    float* matrixPGold = new float[sizeMX * sizeNY];
 
     // LOOK: Setup random number generator and fill host arrays and the scalar a.
     std::random_device rd;
@@ -65,13 +71,34 @@ int main(int argc, char *argv[])
     //     for k -> 0 to sizeXY
     //       dot = m[k, px] * n[py, k]
     //  matrixPGold[py, px] = dot
+    for (int py = 0; py < sizeNY; ++py) {
+        for (int px = 0; px < sizeMX; ++px) {
+            float partial_sum = 0.0f;
+            for (int k = 0; k < sizeXY; ++k) {
+                partial_sum += matrixM[px * sizeXY + k] * matrixN[k * sizeNY + py];
+            }
+            matrixPGold[py * sizeMX + px] = partial_sum;
+        }
+    }
 
     // Device arrays
     float *d_matrixM, *d_matrixN, *d_matrixP;
 
     // TODO 4: Allocate memory on the device for d_matrixM, d_matrixN, d_matrixP.
+    unsigned consume_space_m = sizeMX * sizeXY * sizeof(float);
+    unsigned consume_space_n = sizeXY * sizeNY * sizeof(float);
+    unsigned consume_space_p = sizeMX * sizeNY * sizeof(float);
+    CUDA(cudaMalloc((void**)&d_matrixM, consume_space_m));
+    CUDA(cudaMalloc((void**)&d_matrixN, consume_space_n));
+    CUDA(cudaMalloc((void**)&d_matrixP, consume_space_p));
 
     // TODO 5: Copy array contents of M and N from the host (CPU) to the device (GPU)
+    CUDA(cudaMemcpy(d_matrixM, matrixM, consume_space_m, cudaMemcpyHostToDevice));
+    // Break: pass invalid pointers -> invalid argument
+    // CUDA(cudaMemcpy(nullptr, matrixM, consume_space_m, cudaMemcpyHostToDevice));
+    // Break: out of bound access -> invalid argument
+    // CUDA(cudaMemcpy(d_matrixM, matrixM, consume_space_m * 2, cudaMemcpyHostToDevice));
+    CUDA(cudaMemcpy(d_matrixN, matrixN, consume_space_n, cudaMemcpyHostToDevice));
 
     CUDA(cudaDeviceSynchronize());
 
@@ -85,14 +112,18 @@ int main(int argc, char *argv[])
     // TODO 6: Assign a 2D distribution of BS_X x BS_Y x 1 CUDA threads within
     // Calculate number of blocks along X and Y in a 2D CUDA "grid" using divup
     // HINT: The shape of matrices has no impact on launch configuaration
+    unsigned threadsPerBlockOnX = 32;
+    unsigned threadsPerBlockOnY = 32;
+
     DIMS dims;
-    dims.dimBlock = dim3(1, 1, 1);
-    dims.dimGrid  = dim3(1, 1, 1);
+    dims.dimBlock = dim3(threadsPerBlockOnX, threadsPerBlockOnY, 1);
+    dims.dimGrid  = dim3(divup(sizeMX, threadsPerBlockOnX), divup(sizeNY, threadsPerBlockOnY), 1);
 
     // TODO 7: Launch the matrix transpose kernel
-    // matrixMultiplicationNaive<<<>>>();
+    matrixMultiplicationNaive<<<dims.dimGrid, dims.dimBlock>>>(d_matrixP, d_matrixM, d_matrixN, sizeMX, sizeNY, sizeXY);
 
     // TODO 8: copy the answer back to the host (CPU) from the device (GPU)
+    CUDA(cudaMemcpy(matrixP, d_matrixP, consume_space_p, cudaMemcpyDeviceToHost));
 
     // LOOK: Use compareReferenceAndResult to check the result
     compareReferenceAndResult(matrixPGold, matrixP, sizeMX * sizeNY, 1e-3);
@@ -101,6 +132,9 @@ int main(int argc, char *argv[])
     ////////////////////////////////////////////////////////////
 
     // TODO 9: free device memory using cudaFree
+    CUDA(cudaFree(d_matrixM));
+    CUDA(cudaFree(d_matrixN));
+    CUDA(cudaFree(d_matrixP));
 
     // free host memory
     delete[] matrixM;
